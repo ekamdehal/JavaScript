10172c4d4fd9eb8c97a1c06272193cacf84e2417
Ekamjit Singh <ekamdehal300@gmail.com>
2025-08-11 16:33:46 +0100

    change incoming

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -34,13 +34,12 @@ function renderList() {
     // const dueDate = todoObject.dueDate;
     const { name, dueDate } = todoObject;
     const html = `
-      <p>
-        ${name} ${dueDate}
-        <button onclick="
-            todoList.splice(${i}, 1);
-            renderList();
-        ">Delete</button>
-      </p>
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+          todoList.splice(${i}, 1);
+          renderList();
+      " class="delete-todo-button">Delete</button>
     `;
     todoListHTML += html;
   }
