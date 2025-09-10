0c0b175b1eb7bb218d8a2e3868ba837cba048e0d
ekamdehal <ekamdehal300@gmail.com>
2025-09-10 08:15:20 +0100

    instead of using the DOM when deleting a cart item, the HTML now just regenerates the order summary again

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -115,8 +115,7 @@ export function renderOrderSummary() {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-      const container = document.querySelector(`.js-cart-item-container-${productId}`);
-      container.remove();
+      renderOrderSummary();
       updateCheckoutQuantity();
       renderPaymentSummary();
     });
