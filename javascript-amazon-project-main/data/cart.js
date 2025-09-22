8323e1483b26406436e9e3660c986f395e9efcbc
ekamdehal <ekamdehal300@gmail.com>
2025-09-22 07:31:31 +0100

    created automated tests for addToCart

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,18 +1,16 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart = [];
 
-if (!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '1'
-  }];
+loadFromStorage();
 
-  saveToStorage();
+export function loadFromStorage() {
+  const cartJson = localStorage.getItem('cart');
+
+  if (cartJson) {
+    // fill  the cart with parsed items after clearing it
+    cart.splice(0, cart.length, ...JSON.parse(cartJson));
+  }
 }
+
 
 // Adds a product to the cart
 export function addToCart(productId, selectedQuantity) {
